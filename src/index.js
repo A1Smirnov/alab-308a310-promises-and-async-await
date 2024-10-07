aef48d4695d75c31ebfcfb5dc0246b12de588cff
A1Smirnov <avsmirnov@live.com>
2024-10-07 14:55:51 -0700

    Task resolved, submit return, console.log(inChat)

--- src/index.js
+++ src/index.js
@@ -18,10 +18,16 @@ async function getUserData(id) {
       
       // Parsing user data from db1, db2, db3 based on the dbName returned by central
       const dbs = { db1, db2, db3 };
-      const basicData = await dbs[dbName](id);
+      // Part 2 - change `dbs[dbName](id)` to Promise.all for parallel calls to all 4 db
+      // Max possible timing is about 200 ms - test it
+      // !!! Bugfixed #7 const values
+      const [basicData, personalData] = await Promise.all([
+        dbs[dbName](id), // get base info
+        vault(id)         // get personal info
+      ]);
       
       // Fetch personal user data from the vault
-      const personalData = await vault(id);
+    //   const personalData = await vault(id); - Bugfixed #8
       
       // Data mining into a single object with the following structure
       const userData = {
@@ -74,3 +80,45 @@ async function getUserData(id) {
 //   getUserData(-2) // Invalid ID
 //   .then(data => console.log('User Data:', data))
 //   .catch(err => console.error('Error:', err.message));
+
+//   getUserData(`Robin`) // Invalid ID
+//   .then(data => console.log('User Data:', data))
+//   .catch(err => console.error('Error:', err.message));
+
+
+// ALSO instead of async/await IT CAN BE DONE WITH Promise chaining methods: Promise-then-catch
+
+// Testing perfomance for both methods
+
+async function testPerformance(id) {
+    const startTime = performance.now();  // starTime
+  
+    try {
+      const data = await getUserData(id);  // Get things done
+      const endTime = performance.now();   // endTime
+  
+      const duration = endTime - startTime;  // Calculate spent time
+      console.log(`Time to resolve request for id ${id}: ${duration.toFixed(2)} ms`);
+  
+      if (duration <= 200) {
+        console.log(`Fit in our maxtime of 200 ms. User:`, data);
+      } else {
+        console.warn(`It took too much time (more then 200 ms)! Time: ${duration.toFixed(2)} ms`);
+      }
+  
+    } catch (error) {
+      const endTime = performance.now();  // Check inerror time
+      const duration = endTime - startTime;  // Calc time
+      console.error(`Error for ${id}. Time: ${duration.toFixed(2)} ms`);
+      console.error('Error:', error.message);
+    }
+  }
+
+
+  // Testing perfomance
+testPerformance(1);  // Test with valid ID
+testPerformance(5);  // Test with valid ID
+testPerformance(11); // Test invalid ID
+testPerformance(`Robin`); // Test invalid ID
+
+
