c9ab9c28f0d95c3897fb6945e834810175fdff5e
A1Smirnov <avsmirnov@live.com>
2024-10-07 14:01:57 -0700

    Starting code

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { central, db1, db2, db3, vault } from "./database.js";
+// Importing database functions. DO NOT MODIFY THIS LINE.
+
+// Test envoirnment
+console.log(`Test of Promises and ASync console`)
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+}
